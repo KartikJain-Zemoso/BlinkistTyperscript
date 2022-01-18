03768faa0c4245caa13dc4e35f5e804fae9f82ec
Kartik Jain <kartik.jain@zemosolabs.com>
2022-01-18 19:36:54 +0530

    coverage

--- src/components/molecules/NavItems/index.test.tsx
+++ src/components/molecules/NavItems/index.test.tsx
@@ -4,17 +4,17 @@ import NavItems from ".";
 test("renders NavBar Items", () => {
   render(<NavItems title="My Library" />);
   const linkElement = screen.getByText("My Library");
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeTruthy();
 });
 
 test("renders NavBarItems Exlpore", () => {
   render(<NavItems title="My Library" />);
   const linkElement = screen.getByText("My Library");
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeTruthy();
 });
 
 test("renders NavBar Items Account", () => {
   render(<NavItems title="My Library" />);
   const linkElement = screen.getByText("My Library");
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeTruthy();
 });
